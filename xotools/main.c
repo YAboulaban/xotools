72f3f2ef47bae79c535aeed0f741cd92ffe382c0
YAboulaban <yosifaboulaban@gmail.com>
2024-05-17 20:55:49 +0300

    Initial Commit

---
+++ xotools/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  xotools
+//
+//  Created by Yosif Aboulaban on 17.05.2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
